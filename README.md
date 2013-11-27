5861e0016d898503c287ff0ebc0a9f5e683f49e4
Ben Webb <bjwebb67@googlemail.com>
2013-11-27 14:19:11 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+    git clone git@github.com:Bjwebb/IATI-Stats-Calculated.git stats-calculated
---
+++ plots.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""
+Show how to make date plots in matplotlib using date tick locators and
+formatters.  See major_minor_demo1.py for more information on
+controlling major and minor ticks
+
+All matplotlib date plotting is done by converting date instances into
+days since the 0001-01-01 UTC.  The conversion, tick locating and
+formatting is done behind the scenes so this is most transparent to
+you.  The dates module provides several converter functions date2num
+and num2date
+
+"""
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+import json, re
+for stat_name in ['fail_validation', 'publishers_fail_validation']:
+    git_stats = json.load(open('./stats-calculated/gitaggregate-dated.json'))
+    items = sorted(git_stats.get(stat_name).items())
+    x_values = [ datetime.date(int(x[0:4]), int(x[5:7]), int(x[8:10])).toordinal() for x,y in items ]
+    y_values = [ y for x,y in items ]
+
+    #years    = mdates.YearLocator()   # every year
+    #months   = mdates.MonthLocator()  # every month
+    dateFmt = mdates.DateFormatter('%Y-%m-%d')
+
+    fig, ax = plt.subplots()
+    ax.plot(x_values, y_values)
+
+
+    # format the ticks
+    #ax.xaxis.set_major_locator(years)
+    ax.xaxis.set_major_formatter(dateFmt)
+    #ax.xaxis.set_minor_locator(months)
+
+    #datemin = datetime.date(r.date.min().year, 1, 1)
+    #datemax = datetime.date(r.date.max().year+1, 1, 1)
+    #ax.set_xlim(datemin, datemax)
+
+    # format the coords message box
+    def price(x): return '$%1.2f'%x
+    ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+    ax.format_ydata = price
+    ax.grid(True)
+
+    # rotates and right aligns the x labels, and moves the bottom of the
+    # axes up to make room for them
+    fig.autofmt_xdate()
+
+    plt.savefig('out/{0}.png'.format(stat_name))
+
