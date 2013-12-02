242b7114517d48f962c582c809e11fd7f205e882
Ben Webb <bjwebb67@googlemail.com>
2013-12-02 18:09:25 +0000

    Improve the outputted html

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
     git clone git@github.com:Bjwebb/IATI-Stats-Calculated.git stats-calculated
+
+    mkdir out
+    python plots.py
+    python make_html.py
--- make_html.py
+++ make_html.py
@@ -1,14 +1,19 @@
 from collections import OrderedDict
 import json
 import jinja2
+
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
+jinja_env.filters['url_to_filename'] = lambda x: x.split('/')[-1]
+
 validation_template = jinja_env.get_template('validation.html')
 
 current_stats = {
     'aggregated': json.load(open('./stats-calculated/current/aggregated.json'), object_pairs_hook=OrderedDict),
     'inverted_file': json.load(open('./stats-calculated/current/inverted-file.json'), object_pairs_hook=OrderedDict)
 }
+ckan = json.load(open('./ckan.json'), object_pairs_hook=OrderedDict)
+
 
 with open('out/validation.html', 'w') as fp:
-    fp.write(validation_template.render(current_stats=current_stats).encode('utf-8'))
+    fp.write(validation_template.render(current_stats=current_stats, ckan=ckan).encode('utf-8'))
 
