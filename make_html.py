b2537c49056f79299230c04d2726a5a6e1cbcf7e
Ben Webb <bjwebb67@googlemail.com>
2013-12-06 15:45:05 +0000

    Update to use new inverted stats format

--- make_html.py
+++ make_html.py
@@ -1,17 +1,32 @@
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 import json
 import jinja2
+import copy
+
+def group_files(d):
+    out = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+    for k,v in d.items():
+        for k2,v2 in v.items():
+            if type(v2) == list:
+                for listitem in v2:
+                    publisher = listitem.split('-', 1)[0]
+                    out[k][k2][publisher].append(listitem)
+            else:
+                out[k][k2] = v2
+    return out
+            
 
 current_stats = {
     'aggregated': json.load(open('./stats-calculated/current/aggregated.json'), object_pairs_hook=OrderedDict),
     'inverted_file': json.load(open('./stats-calculated/current/inverted-file.json'), object_pairs_hook=OrderedDict)
 }
+current_stats['inverted_file_grouped'] = group_files(current_stats['inverted_file'])
 ckan = json.load(open('./ckan.json'), object_pairs_hook=OrderedDict)
 
-def iati_stats_page(template):
+def iati_stats_page(template, **kwargs):
     def f(jinja_env):
         validation_template = jinja_env.get_template(template)
-        return validation_template.render(current_stats=current_stats, ckan=ckan, validation=True)
+        return validation_template.render(current_stats=current_stats, ckan=ckan, **kwargs)
     return f
 
 
@@ -19,7 +34,7 @@ def iati_stats_page(template):
 import github.web
 urls = {
     'index.html': iati_stats_page('index.html'),
-    'validation.html': iati_stats_page('validation.html'),
+    'validation.html': iati_stats_page('validation.html', validation=True),
     'github.html': github.web.main
 }
 
