10b3bc30e3805340969837e296043848aabe045e
Ben Webb <bjwebb67@googlemail.com>
2014-01-06 16:30:45 +0000

    Pull download history from gist properly

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-mkdir data/downloads/
-wget "https://gist.github.com/Bjwebb/8058775/raw/" -O data/downloads/history.csv
+mkdir -p data/downloads/
 wget "https://gist.github.com/Bjwebb/6726204/raw/errors" -O data/downloads/errors
+
 python fetch_data.py
+
+cd data/downloads
+if [ ! -d ./6726204 ]; then
+    git clone https://gist.github.com/6726204.git
+fi
+cd ./6726204
+git checkout master > /dev/null
+git pull > /dev/null
+for commit in `git log --format=format:%H`; do
+    git checkout $commit
+    date=`git log -1 --format="%ai"`
+    count=`cat errors | grep -v '^\.$' | wc -l`
+    echo $date,$count
+done > ../history.csv
+
