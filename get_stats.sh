9b088b2ce0149b12b1fbda13cabb4e40c20abfab
Ben Webb <bjwebb67@googlemail.com>
2014-03-20 12:37:03 +0000

    Update get_stats.sh to get new stats, fixes #99

--- get_stats.sh
+++ get_stats.sh
@@ -1,8 +1,11 @@
 mkdir stats-calculated
-for f in gitaggregate-dated ckan gitdate; do
+for f in ckan gitdate; do
     curl --compress "http://arstneio.com/iati/stats/${f}.json" > stats-calculated/${f}.json
 done
 
 cd stats-calculated
 wget "http://arstneio.com/iati/stats/current.tar.gz" -O current.tar.gz
+wget "http://arstneio.com/iati/stats/gitaggregate-dated.tar.gz" -O gitaggregate-dated.tar.gz
 tar -xvf current.tar.gz
+tar -xvf gitaggregate-dated.tar.gz 
+
