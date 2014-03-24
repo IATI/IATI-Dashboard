dfe03a3ed625d9c722256007239c0ee06b26591a
Ben Webb <bjwebb67@googlemail.com>
2014-03-24 14:23:04 +0000

    [#93] Stop using a gh-pages for output

--- git.sh
+++ git.sh
@@ -1,14 +1,10 @@
-cd out
-git reset --hard HEAD
-git pull
-rm -r *
-cd ..
+rm -rf out
+mkdir out
 ./fetch_data.sh || exit 1
 python plots.py || exit 1
 python make_csv.py || exit 1
 python make_html.py || exit 1
 cp favicon.png out/
-cd out
-git add .
-git commit -a -m 'Auto'
-git push
+mv web web.1
+mv out web
+rm -rf web.1
