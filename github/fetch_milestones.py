370e83e84cddcc678a20d755cff129ab1532ef4e
Ben Webb <bjwebb67@googlemail.com>
2014-06-05 16:30:08 +0100

    Handle unicode github responses properly

--- github/fetch_milestones.py
+++ github/fetch_milestones.py
@@ -17,12 +17,12 @@ def fetch():
 
     r = requests.get('https://api.github.com/orgs/IATI/repos',auth=HTTPBasicAuth(user,password))
     with open('data/github/repo.json', 'w') as fp:
-        fp.write(r.text)
+        fp.write(r.text.encode('utf-8'))
     for repo in json.loads(r.text):
         milestones_url = repo['milestones_url'].replace('{/number}','')
         milestones_request = requests.get(milestones_url,auth=HTTPBasicAuth(user,password))
         with open('data/github/milestones/{0}.json'.format(repo['name']), 'w') as fp:
-            fp.write(milestones_request.text)
+            fp.write(milestones_request.text.encode('utf-8'))
 
         issues_url = repo['issues_url'].replace('{/number}','')
         issues_no_milestone_request = requests.get(issues_url, params={'milestone':'none'},auth=HTTPBasicAuth(user,password))
@@ -38,7 +38,7 @@ def fetch():
         milestones_request = requests.get(milestones_closed_url,auth=HTTPBasicAuth(user,password))
         with open('data/github/milestones_closed/{0}.json'.format(repo['name']), 'w') as fp:
         #with open('data/github/milestones_closed/urls') as fp:
-            fp.write(milestones_request.text)
+            fp.write(milestones_request.text.encode('utf-8'))
             #fp.write(milestones_closed_url)
 
 
