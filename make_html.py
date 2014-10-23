b8141a995486b7aafa6d9f54fcd1ab4514c9bacd
Ben Webb <bjwebb67@googlemail.com>
2014-10-23 14:56:32 +0100

    [#247] Stop hardcoding years in forwardlooking template

--- make_html.py
+++ make_html.py
@@ -204,6 +204,8 @@ slugs = {
 }
 app.jinja_env.globals['slugs'] = slugs
 
+this_year = datetime.date.today().year
+
 from vars import expected_versions
 import github.web, licenses, timeliness
 app.jinja_env.filters['has_future_transactions'] = timeliness.has_future_transactions
@@ -240,7 +242,7 @@ urls = {
 #    'coverage.html': iati_stats_page('coverage.html', page='coverage', dac2012=dac2012, float=float),
     'timeliness.html': iati_stats_page('timeliness.html', page='timeliness', timeliness=timeliness),
     'timeliness_timelag.html': iati_stats_page('timeliness_timelag.html', page='timeliness', timeliness=timeliness),
-    'forwardlooking.html': iati_stats_page('forwardlooking.html', page='forwardlooking'),
+    'forwardlooking.html': iati_stats_page('forwardlooking.html', page='forwardlooking', years=map(str, range(this_year, this_year+3))),
     'files.html': iati_stats_page('files.html', page='files', firstint=firstint),
     'activities.html': iati_stats_page('activities.html', page='activities'),
     'download.html': iati_stats_page('download.html', page='download'),
