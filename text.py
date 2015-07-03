95bf6865dbeff73dbac6266b6e97b2431b4d69a5
dalepotter <dalepotter@gmail.com>
2015-07-03 15:05:08 +0100

    Turn on Comprehensiveness pages

--- text.py
+++ text.py
@@ -109,5 +109,5 @@ navigation = {
     'data_quality': ['download', 'xml', 'validation', 'versions', 'licenses', 'organisation', 'identifiers', 'reporting_orgs'],
     'exploring_data': ['elements', 'codelists', 'booleans', 'dates'],
     'github': ['github', 'milestones', 'milestones-completed'],
-    'publishing_stats': ['timeliness', 'forwardlooking']#, 'comprehensiveness'] #, 'coverage' ]
+    'publishing_stats': ['timeliness', 'forwardlooking', 'comprehensiveness'] #, 'coverage' ]
 }
