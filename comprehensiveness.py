1658e6a384c76efa8160f30ff8809ab92c8d9c7e
dalepotter <dalepotter@gmail.com>
2015-07-17 16:31:07 +0100

    Improve UI

--- comprehensiveness.py
+++ comprehensiveness.py
@@ -38,7 +38,7 @@ columns = {
         ('activity-website', 'Activity Website', 1),
         ('title_recipient_language', 'Recipient Language (TBC)', 0),
         ('conditions_attached', 'Conditions Attached', 1),
-        ('result_indicator', 'Result/Indicator', 1),
+        ('result_indicator', 'Result/ Indicator', 1),
         ('valueadded_average', 'Average', 0), # don't include the average in the average
     ]}
 column_headers = {tabname:[x[1] for x in values] for tabname, values in columns.items()}
