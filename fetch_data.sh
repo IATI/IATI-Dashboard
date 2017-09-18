94727b67b2a82a697e9250a36e7352a5cbe6252f
Hayden <hayden.field@devinit.org>
2017-09-18 10:48:33 +0100

    Add output to fetch_data.sh
    This script is timing out due to hitting a 10 minute timer.
    Outputting something may help.
    
    It not, there is a command that can be set in travis.yml

--- fetch_data.sh
+++ fetch_data.sh
@@ -10,10 +10,12 @@ python fetch_data.py
 # Generate a csv file with the number of download errors logged since 2013
 # This is taken from the gist on the 'iati-bot' account: https://gist.github.com/iati-bot
 cd data/downloads
+echo "cloning download errors"
 if [ ! -d ./4f86dc7b36562c8b2b21 ]; then
     git clone https://gist.github.com/4f86dc7b36562c8b2b21.git
 fi
 cd ./4f86dc7b36562c8b2b21
+echo "cloned download errors - checking out commits"
 git checkout master > /dev/null
 git pull > /dev/null
 for commit in `git log --format=format:%H`; do
@@ -22,23 +24,32 @@ for commit in `git log --format=format:%H`; do
     count=`cat errors | grep -v '^\.$' | wc -l`
     echo $date,$count
 done > ../history.csv
+echo "cloned and checked out download errors"
 cd ../../../
 
 # Get codelists for versions v1.x and v2.x of the IATI Standard
 cd data
+echo "cloning Codelists-1"
 if [ ! -d IATI-Codelists-1 ]; then
     git clone https://github.com/IATI/IATI-Codelists.git IATI-Codelists-1
 fi
 cd IATI-Codelists-1
+echo "checking out Codelists-1"
 git checkout version-1.05 > /dev/null
 git pull > /dev/null
+echo "running gen.sh for Codelist-1"
 ./gen.sh
 
 cd ..
+echo "cloning Codelists-2"
 if [ ! -d IATI-Codelists-2 ]; then
     git clone https://github.com/IATI/IATI-Codelists.git IATI-Codelists-2
 fi
 cd IATI-Codelists-2
+echo "checking out Codelists-1"
 git checkout version-2.02 > /dev/null
 git pull > /dev/null
+echo "running gen.sh for Codelist-2"
 ./gen.sh
+
+echo "completed fetching data"
