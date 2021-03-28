c54703705fd674aa2b429dcbdb5282a05f97338a
Andy Lulham <a.lulham@gmail.com>
2021-03-28 22:12:27 +0100

    Minor refactor

--- fetch_data.sh
+++ fetch_data.sh
@@ -27,29 +27,23 @@ done > ../history.csv
 echo "cloned and checked out download errors"
 cd ../../../
 
-# Get codelists for versions v1.x and v2.x of the IATI Standard
 cd data
+
+# Get codelists for versions v1.x and v2.x of the IATI Standard
 echo "cloning Codelists-1"
-if [ ! -d IATI-Codelists-1 ]; then
-    git clone https://github.com/IATI/IATI-Codelists.git IATI-Codelists-1
-fi
+rm -rf IATI-Codelists-1
+git clone --branch version-1.05 https://github.com/IATI/IATI-Codelists.git IATI-Codelists-1
 cd IATI-Codelists-1
-echo "checking out Codelists-1"
-git checkout version-1.05 > /dev/null
-git pull > /dev/null
 echo "running gen.sh for Codelist-1"
 ./gen.sh
-
 cd ..
+
 echo "cloning Codelists-2"
-if [ ! -d IATI-Codelists-2 ]; then
-    git clone https://github.com/andylolz/IATI-Codelists.git IATI-Codelists-2
-fi
+rm -rf IATI-Codelists-2
+git clone --branch version-2.03 https://github.com/andylolz/IATI-Codelists.git IATI-Codelists-2
 cd IATI-Codelists-2
-echo "checking out Codelists-2"
-git checkout version-2.03 > /dev/null
-git pull > /dev/null
 echo "running gen.sh for Codelist-2"
 ./gen.sh
+cd ..
 
 echo "completed fetching data"
