ea713003846aa3e373ba293c930bcee5f66e7055
Andy Lulham <a.lulham@gmail.com>
2021-05-03 17:38:10 +0100

    Reinstate tests

--- tests/test_comprehensiveness.py
+++ tests/test_comprehensiveness.py
@@ -1,23 +1,25 @@
 import sys
 from os import path
-sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 import comprehensiveness
 
 mock_stats = {
     'comprehensiveness': {
-      'activity-date': 2,
-      'activity-status': 2,
-      'recipient_language': 0,
-      'transaction_spend': 1,
-      },
+        'activity-date': 2,
+        'activity-status': 2,
+        'recipient_language': 0,
+        'transaction_spend': 1,
+    },
     'comprehensiveness_denominator_default': 2,
     'comprehensiveness_denominators': {
-      'recipient_language': 0,
-      'transaction_spend': 1,
-      'transaction_traceability': 1
-      }
+        'recipient_language': 0,
+        'transaction_spend': 1,
+        'transaction_traceability': 1
     }
+}
+
 
 def test_denominator():
     assert comprehensiveness.denominator('activity-date', mock_stats) == 2
