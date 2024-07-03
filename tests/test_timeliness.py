9dfe36f2a75b519971d040a3feb0081b48c1e134
Chris Arridge <chris.arridge@opendataservices.coop>
2024-07-03 19:00:56 +0100

    fix: Fixed new test formatting to pass flake8
    Fixed a few flake8 formatting issues.

--- tests/test_timeliness.py
+++ tests/test_timeliness.py
@@ -10,6 +10,7 @@ import timeliness  # noqa: E402
 
 
 def test_short_month():
-    month_strings = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-    for index,s in enumerate(month_strings):
-        assert timeliness.short_month('01-{:02d}-2024'.format(index+1)) == s
+    month_strings = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                     'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    for index, s in enumerate(month_strings):
+        assert timeliness.short_month('01-{:02d}-2024'.format(index + 1)) == s
