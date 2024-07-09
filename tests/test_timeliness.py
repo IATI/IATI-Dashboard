9b0c2a815a68dfcdd46735fe7191741143000ef8
Chris Arridge <chris.arridge@opendataservices.coop>
2024-07-09 15:30:56 +0100

    fix: Fixed flake8 formatting in test

--- tests/test_timeliness.py
+++ tests/test_timeliness.py
@@ -15,18 +15,19 @@ def test_short_month():
     for index, s in enumerate(month_strings):
         assert timeliness.short_month('01-{:02d}-2024'.format(index + 1)) == s
 
+
 def test_parse_iso_date():
     test_date_1 = timeliness.parse_iso_date("2024-01-01")
-    assert test_date_1.year==2024
-    assert test_date_1.month==1
-    assert test_date_1.day==1
+    assert test_date_1.year == 2024
+    assert test_date_1.month == 1
+    assert test_date_1.day == 1
 
     test_date_2 = timeliness.parse_iso_date("2024-02-29")
-    assert test_date_2.year==2024
-    assert test_date_2.month==2
-    assert test_date_2.day==29
+    assert test_date_2.year == 2024
+    assert test_date_2.month == 2
+    assert test_date_2.day == 29
 
     test_date_3 = timeliness.parse_iso_date("2024-04-01")
-    assert test_date_3.year==2024
-    assert test_date_3.month==4
-    assert test_date_3.day==1
+    assert test_date_3.year == 2024
+    assert test_date_3.month == 4
+    assert test_date_3.day == 1
