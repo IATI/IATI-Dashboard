3c65a7c2dca5a094567c14a3d84aebe6ae8bc838
Bee Webb <bee.webb@opendataservices.coop>
2025-04-29 16:31:18 +0000

    feat: Add a Dataset model, and populate in dashboard_import
    https://github.com/IATI/IATI-Dashboard/issues/669

--- iati_dashboard/management/commands/dashboard_import.py
+++ iati_dashboard/management/commands/dashboard_import.py
@@ -1,15 +1,17 @@
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
-from ... import comprehensiveness, forwardlooking, humanitarian, summary_stats, timeliness
-from ...data import current_stats, get_publisher_stats, publishers_ordered_by_title
-from ...models import Publisher
+from ... import comprehensiveness, filepaths, forwardlooking, humanitarian, summary_stats, timeliness
+from ...data import JSONDir, ckan, current_stats, get_publisher_stats, publishers_ordered_by_title
+from ...models import Dataset, Publisher
 
 
 class Command(BaseCommand):
     @transaction.atomic
     def handle(self, *args, **options):
         Publisher.objects.all().delete()
+        Dataset.objects.all().delete()
+
         for publisher_title, publisher_slug in publishers_ordered_by_title:
             stats_json = dict(get_publisher_stats(publisher_slug))
             publisher = Publisher(
@@ -27,3 +29,24 @@ class Command(BaseCommand):
             )
             publisher.summary_stats = summary_stats.generate_row(publisher)
             publisher.save()
+
+        for publisher_short_name, datasets_dict in ckan.items():
+            for dataset_short_name, dataset_dict in datasets_dict.items():
+                print(dataset_short_name)
+                stats_json = dict(
+                    JSONDir(
+                        filepaths.join_stats_path(
+                            f"current/aggregated-file/{publisher_short_name}/{dataset_short_name}"
+                        )
+                    )
+                )
+                try:
+                    dataset = Dataset(
+                        publisher=Publisher.objects.get(short_name=publisher_short_name),
+                        short_name=dataset_short_name,
+                        source_url=dataset_dict["resource"]["url"],
+                        stats_json=stats_json,
+                    )
+                    dataset.save()
+                except Publisher.DoesNotExist:
+                    print("Publisher", publisher_short_name, "not found")
