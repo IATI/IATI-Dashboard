e860ac09095f986d5cd9cb5edbeb27ad0e3e30b3
Bee Webb <bee.webb@opendataservices.coop>
2025-04-30 16:08:57 +0000

    refactor: Add element_slug function to replace slugs.element.by_i

--- iati_dashboard/data.py
+++ iati_dashboard/data.py
@@ -312,13 +312,17 @@ except IOError:
     dac2012 = {}
 
 
+def element_slug(element):
+    return re.sub(r"{[^}]*}", "", element.replace("{http://www.w3.org/XML/1998/namespace}", "xml:").replace("/", "_"))
+
+
 def make_slugs(keys):
     out = {"by_slug": {}, "by_i": {}}
     for i, key in enumerate(keys):
         slug = re.sub(
             r"[^a-zA-Z0-9:@\-_]",
             "",
-            re.sub(r"{[^}]*}", "", key.replace("{http://www.w3.org/XML/1998/namespace}", "xml:").replace("/", "_")),
+            element_slug(key),
         ).strip("_")
         while slug in out["by_slug"]:
             slug += "_"
