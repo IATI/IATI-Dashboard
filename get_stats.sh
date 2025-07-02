4f7eb2aa14a365149c1597bb580e4889a5625424
Bee Webb <bee.webb@opendataservices.coop>
2025-07-02 10:14:34 +0000

    ati: Download the stats from the dev copy with ati tests included
    Probably revert this after the stats change goes live.

--- get_stats.sh
+++ get_stats.sh
@@ -3,13 +3,13 @@ set -eux
 
 mkdir stats-calculated
 for f in ckan gitdate licenses; do
-    curl --compressed "https://dashboard.iatistandard.org/stats/${f}.json" > stats-calculated/${f}.json
+    curl --compressed "https://ati.dev.merged.dashboard.iatistandard.org/stats/${f}.json" > stats-calculated/${f}.json
 done
 
 cd stats-calculated
-wget "https://dashboard.iatistandard.org/stats/current.tar.gz" -O current.tar.gz
-wget "https://dashboard.iatistandard.org/stats/gitaggregate-dated.tar.gz" -O gitaggregate-dated.tar.gz
-wget "https://dashboard.iatistandard.org/stats/gitaggregate-publisher-dated.tar.gz" -O gitaggregate-publisher-dated.tar.gz
+wget "https://ati.dev.merged.dashboard.iatistandard.org/stats/current.tar.gz" -O current.tar.gz
+wget "https://ati.dev.merged.dashboard.iatistandard.org/stats/gitaggregate-dated.tar.gz" -O gitaggregate-dated.tar.gz
+wget "https://ati.dev.merged.dashboard.iatistandard.org/stats/gitaggregate-publisher-dated.tar.gz" -O gitaggregate-publisher-dated.tar.gz
 tar -xf current.tar.gz
 tar -xf gitaggregate-dated.tar.gz
 tar -xf gitaggregate-publisher-dated.tar.gz
