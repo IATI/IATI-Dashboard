ce275a2b66afd060c0cbac674cc123288660cd26
Bee Webb <bee.webb@opendataservices.coop>
2025-07-02 11:35:18 +0000

    templates: Move templates dir to jinja2, so automatic ADD_DIRS works
    The django Jinja2 backend looks for a "jinja2" directory, not
    "templates". By changing the directory we can use the automatic
    "APP_DIRS" detection, instead of needing to specify it in "DIRS"
    explicitly.
    
    More importantly, we can enable "APP_DIRS" for the DjangoTemplates
    backend, and it won't find our jinja2 templates, but will find the
    Django Rest Framework default html templates. Therefore we can remove
    the setting that enables only the JSON output.

--- iati_dashboard/settings.py
+++ iati_dashboard/settings.py
@@ -86,7 +86,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [],
-        "APP_DIRS": False,
+        "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
@@ -96,7 +96,6 @@ TEMPLATES = [
     },
     {
         "BACKEND": "django.template.backends.jinja2.Jinja2",
-        "DIRS": [BASE_DIR / "iati_dashboard" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -184,7 +183,4 @@ STORAGES = {
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 50,
-    "DEFAULT_RENDERER_CLASSES": [
-        "rest_framework.renderers.JSONRenderer",
-    ],
 }
