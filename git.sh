223e146e8366fb3839cd3bc59b0163aefaf28f06
Bee Webb <bee.webb@opendataservices.coop>
2025-07-16 14:02:07 +0000

    feat: Use set -eux in bash scripts consistenly
    (to ensure they exit with error if something goes wrong)
    
    https://github.com/IATI/IATI-Dashboard/issues/757

--- git.sh
+++ git.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
+set -eux
+# ^ https://explainshell.com/explain?cmd=set+-eux
 
 echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Starting Dashboard generation"
 
 echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Removing 'out' directory and creating a new one"
-rm -rf out
+rm -rf out || true
 mkdir out
 
 echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Fetching data"
-./fetch_data.sh &> fetch_data.log || exit 1
+./fetch_data.sh &> fetch_data.log
 
 echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Running manage.py dashboard_import"
 python manage.py dashboard_import
 
 echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Running make_plots.py"
-python -m iati_dashboard.make_plots || exit 1
+python -m iati_dashboard.make_plots
 
 echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Running make_csv.py"
-python manage.py make_csv || exit 1
+python manage.py make_csv
 
 echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Running speakers_kit.py"
-python -m iati_dashboard.speakers_kit || exit 1
+python -m iati_dashboard.speakers_kit
 
 echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Make a backup of the old web directory and make new content live"
-rsync -a --delete web web.bk
-mv web web.1
+rm -rf web.1 || true # web.1 may not exist
+mv web web.1 || true # web may not exist
 mv out web
-rm -rf web.1
 
 echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Dashboard generation complete"
