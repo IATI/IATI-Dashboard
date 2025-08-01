3cb44c848aa496aa1a819b2ca304a3fd240b16cb
Bee Webb <bee.webb@opendataservices.coop>
2025-08-01 08:40:19 +0000

    tests: End to end tests of recipient country filtering
    https://github.com/IATI/IATI-Dashboard/issues/727

--- end_to_end_tests/tests.py
+++ end_to_end_tests/tests.py
@@ -14,9 +14,38 @@ def firefox_options(firefox_options):
 
 
 def test_home_page(selenium):
-    selenium.get("http://localhost:8000/")
+    root_url = "http://localhost:8000"
+
+    selenium.get(root_url)
     assert "Dashboard Home" in selenium.find_element("tag name", "body").text
+
+    selenium.get(f"{root_url}/errors/xml-errors/")
+    xml_errors_body = selenium.find_element("tag name", "body").text
+    selenium.get(f"{root_url}/errors/validation/")
+    schema_validation_body = selenium.find_element("tag name", "body").text
+
+    # Check that we're picking up bad xml and schema validation
+    assert "test_ro_1-badxml" in xml_errors_body
+    assert "test_ro_1-empty_activity" in schema_validation_body
+    assert "test_ro_1-valid_activity" not in xml_errors_body
+    assert "test_ro_1-valid_activity" not in schema_validation_body
+
+    # Check that these activities validate against the schema,
+    # so we know we've not typoed the elements/attributes we're interested in
+    assert "in_ao_1-activities" not in xml_errors_body
+    assert "in_ao_1-activities" not in schema_validation_body
+    assert "in_ao_2-activities" not in xml_errors_body
+    assert "in_ao_2-activities" not in schema_validation_body
+
     selenium.find_element("link text", "Reporting Orgs").click()
     assert selenium.current_url.endswith("/publishers/")
+
+    # TODO click the interface to get to this page
+    selenium.get(f"{root_url}/publishers/?recipient_country_code=AO")
     assert selenium.title == "IATI Dashboard – IATI Reporting Orgs"
-    assert "Reporting orgs in query: 1" in selenium.find_element("tag name", "body").text
+    body_text = selenium.find_element("tag name", "body").text
+    assert "Reporting orgs in query: 2" in body_text
+    # Test recipient-country under iati-activity
+    assert "in_ao_1" in body_text
+    # Test recipient-country under transaction
+    assert "in_ao_2" in body_text
