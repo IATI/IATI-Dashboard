b5175c51dcc27c7d654cca69b2f735c9c1e258fc
Bee Webb <bee.webb@opendataservices.coop>
2025-08-04 11:39:38 +0000

    api: Allow very large custom page_size
    This allows people to fetch all reporting orgs or datasets in one go.
    
    https://github.com/IATI/IATI-Dashboard/issues/759

--- iati_dashboard/api/views.py
+++ iati_dashboard/api/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 from ..filters import ReportingOrgFilter
 from ..models import Dataset, ReportingOrg
 from .serializers import DatasetSerializer, ReportingOrgSerializer
+
+
+class LargeMaxPageNumberPagination(PageNumberPagination):
+    page_size = 50
+    page_size_query_param = "page_size"
+    max_page_size = 1_000_000
 
 
 class ReportingOrgViewSet(viewsets.ReadOnlyModelViewSet):
@@ -15,6 +22,7 @@ class ReportingOrgViewSet(viewsets.ReadOnlyModelViewSet):
 
     queryset = ReportingOrg.objects.defer("stats_json").all().order_by("human_readable_name")
     serializer_class = ReportingOrgSerializer
+    pagination_class = LargeMaxPageNumberPagination
     filter_backends = (DjangoFilterBackend,)
     filterset_class = ReportingOrgFilter
 
@@ -29,8 +37,14 @@ class DatasetViewSet(viewsets.ReadOnlyModelViewSet):
     API endpoint that allows reporting orgs to be viewed.
     """
 
-    queryset = Dataset.objects.all().order_by("short_name")
+    queryset = (
+        Dataset.objects.all()
+        .select_related("reporting_org")
+        .only("short_name", "source_url", "registry_metadata_realtime", "reporting_org__short_name")
+        .order_by("short_name")
+    )
     serializer_class = DatasetSerializer
+    pagination_class = LargeMaxPageNumberPagination
 
     def retrieve(self, request, pk: str):
         user = get_object_or_404(self.queryset, short_name=pk)
--- iati_dashboard/settings.py
+++ iati_dashboard/settings.py
@@ -191,11 +191,6 @@ STORAGES = {
     },
 }
 
-REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 50,
-}
-
 
 REGISTRY_UPDATE_PROCESSOR = {
     "AZ_SERVICE_BUS_CONNECTION_STRING": env("AZ_SERVICE_BUS_CONNECTION_STRING"),
