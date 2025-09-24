2ba27c157cc2ebe6d2d79956f393ac2257d82faf
Bee Webb <bee.webb@opendataservices.coop>
2025-09-24 10:43:42 +0000

    ci: download partial Bulk Data Service metadata for unit tests
    https://github.com/IATI/IATI-Dashboard/issues/768

--- partial_stats_for_unit_tests.sh
+++ partial_stats_for_unit_tests.sh
@@ -23,6 +23,11 @@ for file in licenses.json gitdate.json; do
 done
 curl --compressed https://$domain/stats/ckan.json | jq "{$publisher_short_name: .$publisher_short_name}" > stats-calculated/ckan.json
 
+cp stats-calculated/current/bulk-data-service-metadata/reporting-orgs.json stats-calculated/current/bulk-data-service-metadata/reporting-orgs.json.all
+cat stats-calculated/current/bulk-data-service-metadata/reporting-orgs.json.all | jq "{index_created, index_created_unix_timestamp, reporting_orgs: [.reporting_orgs[]|select(.short_name==\"$publisher_short_name\")]}" > stats-calculated/current/bulk-data-service-metadata/reporting-orgs.json
+cp stats-calculated/current/bulk-data-service-metadata/datasets-full.json stats-calculated/current/bulk-data-service-metadata/datasets-full.json.all
+cat stats-calculated/current/bulk-data-service-metadata/datasets-full.json.all | jq "{index_created, index_created_unix_timestamp, datasets: [.datasets[]|select(.reporting_org_short_name==\"$publisher_short_name\")]}" > stats-calculated/current/bulk-data-service-metadata/datasets-full.json
+
 cat stats-calculated/current/inverted-publisher/activities.json  | jq "{$publisher_short_name: .$publisher_short_name}" > activities.json
 mv activities.json stats-calculated/current/inverted-publisher/activities.json
 rm stats-calculated/current/aggregated-publisher/*/index.html*
