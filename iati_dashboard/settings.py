d960445e2355763f7e431156c550f64463993b5f
Bee Webb <bee.webb@opendataservices.coop>
2025-11-28 16:02:23 +0000

    dashboard: Respond to requests on test. and www.iatiregistry.org

--- iati_dashboard/settings.py
+++ iati_dashboard/settings.py
@@ -26,7 +26,7 @@ env = environ.Env(  # set default values and casting
     SECRET_KEY=(str, secret_key),
     SENTRY_DSN=(str, None),
     USE_X_FORWARDED_HOST=(bool, False),
-    ALLOWED_HOSTS=(list, [".dashboard.iatistandard.org", "iatiregistry.org", "localhost", "127.0.0.1"]),
+    ALLOWED_HOSTS=(list, [".dashboard.iatistandard.org", ".iatiregistry.org", "localhost", "127.0.0.1"]),
     # Allow api features to only be enabled on a dev instance for now
     # This means we can keep it off live until we assess the performance implications
     ENABLE_API_ALPHA=(bool, False),
