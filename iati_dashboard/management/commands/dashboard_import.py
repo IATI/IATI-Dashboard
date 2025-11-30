d148fc77ebd25e8924e9f5001d24d6a732dfa6e8
Bee Webb <bee.webb@opendataservices.coop>
2025-11-30 15:21:48 +0000

    wip don't clear db overnight

--- iati_dashboard/management/commands/dashboard_import.py
+++ iati_dashboard/management/commands/dashboard_import.py
@@ -34,57 +34,85 @@ def file_types(stats_json):
     return types
 
 
+def import_reporting_org_metadata():
+    keep_ids = []
+    for reporting_org_dict in metadata_reporting_orgs["reporting_orgs"]:
+        reporting_org, created = ReportingOrg.objects.get_or_create(id=reporting_org_dict["id"])
+        keep_ids.append(reporting_org_dict["id"])
+        reporting_org.human_readable_name = reporting_org_dict["human_readable_name"]
+        reporting_org.short_name = reporting_org_dict["short_name"]
+        reporting_org.metadata_json = reporting_org_dict
+        reporting_org.save()
+    for reporting_org in ReportingOrg.objects.all():
+        if id not in keep_ids:
+            reporting_org.delete()
+
+
+def import_dataset_metadata():
+    keep_ids = []
+    for dataset_dict in metadata_datasets["datasets"]:
+        dataset, created = Dataset.objects.get_or_create(id=dataset_dict["id"])
+        keep_ids.append(dataset_dict["id"])
+        dataset.reporting_org = ReportingOrg.objects.get(short_name=dataset_dict["reporting_org_short_name"])
+        dataset.short_name = dataset_dict["short_name"]
+        dataset.source_url = dataset_dict["source_url"]
+        dataset.metadata_json = dataset_dict
+        try:
+            dataset.save()
+        except ReportingOrg.DoesNotExist:
+            print(f"ReportingOrg {dataset_dict["reporting_org_short_name"]} not found to link dataset to")
+    for dataset in Dataset.objects.all():
+        if id not in keep_ids:
+            dataset.delete()
+
+def import_stats():
+    for reporting_org_dict in metadata_reporting_orgs["reporting_orgs"]:
+        stats_json = dict(get_publisher_stats(reporting_org_dict["short_name"]))
+        if not stats_json:
+            stats_json = DEFAULT_STATS_JSON
+        try:
+            reporting_org = ReportingOrg.objects.get(id=reporting_org_dict["id"])
+        except ReportingOrg.DoesNotExist:
+            print(f"ReportingOrg {reporting_org_dict["id"]} ({reporting_org_dict["short_name"]}) not found")
+            continue
+        # reporting_org.stats_metadata_json = reporting_org_dict
+        reporting_org.stats_json = stats_json
+        reporting_org.has_future_transactions = timeliness.has_future_transactions(reporting_org_dict["short_name"])
+        reporting_org.timeliness_frequency = timeliness.publisher_frequency_generate_row(reporting_org_dict["short_name"])
+        reporting_org.forwardlooking = forwardlooking.generate_row(reporting_org_dict["short_name"])
+        reporting_org.comprehensiveness = comprehensiveness.generate_row(reporting_org_dict["short_name"])
+        reporting_org.validation_datasets = current_stats["inverted_file_publisher"].get(reporting_org_dict["short_name"], {}).get("validation", {}).get("fail", {})
+        reporting_org.recipient_country_code = recipient_country_code(stats_json)
+        reporting_org.file_types = file_types(stats_json)
+        reporting_org.save()
+        reporting_org.humanitarian = humanitarian.generate_row(reporting_org)
+        reporting_org.summary_stats = summary_stats.generate_row(reporting_org)
+        reporting_org.save()
+
+    for dataset_dict in metadata_datasets["datasets"]:
+        stats_json = dict(
+            JSONDir(
+                filepaths.join_stats_path(
+                    f"current/aggregated-file/{dataset_dict["reporting_org_short_name"]}/{dataset_dict["short_name"]}"
+                )
+            )
+        )
+        if not stats_json:
+            stats_json = DEFAULT_STATS_JSON
+        try:
+            dataset = Dataset.objects.get(id=dataset_dict["id"])
+        except Dataset.DoesNotExist:
+            print(f"Dataset {dataset_dict["id"]} ({dataset_dict["short_name"]}) not found")
+            continue
+        dataset.stats_json = stats_json
+        # dataset.stats_metadata_json = dataset_dict
+        dataset.save()
+
+
 class Command(BaseCommand):
-    @transaction.atomic
     def handle(self, *args, **options):
-        ReportingOrg.objects.all().delete()
-        Dataset.objects.all().delete()
-
-        for reporting_org_dict in metadata_reporting_orgs["reporting_orgs"]:
-            stats_json = dict(get_publisher_stats(reporting_org_dict["short_name"]))
-            if not stats_json:
-                stats_json = DEFAULT_STATS_JSON
-            reporting_org = ReportingOrg(
-                id=reporting_org_dict["id"],
-                human_readable_name=reporting_org_dict["human_readable_name"],
-                short_name=reporting_org_dict["short_name"],
-                metadata_json=reporting_org_dict,
-                stats_json=stats_json,
-                has_future_transactions=timeliness.has_future_transactions(reporting_org_dict["short_name"]),
-                timeliness_frequency=timeliness.publisher_frequency_generate_row(reporting_org_dict["short_name"]),
-                forwardlooking=forwardlooking.generate_row(reporting_org_dict["short_name"]),
-                comprehensiveness=comprehensiveness.generate_row(reporting_org_dict["short_name"]),
-                validation_datasets=current_stats["inverted_file_publisher"]
-                .get(reporting_org_dict["short_name"], {})
-                .get("validation", {})
-                .get("fail", {}),
-                recipient_country_code=recipient_country_code(stats_json),
-                file_types=file_types(stats_json),
-            )
-            reporting_org.save()
-            reporting_org.humanitarian = humanitarian.generate_row(reporting_org)
-            reporting_org.summary_stats = summary_stats.generate_row(reporting_org)
-            reporting_org.save()
-
-        for dataset_dict in metadata_datasets["datasets"]:
-            stats_json = dict(
-                JSONDir(
-                    filepaths.join_stats_path(
-                        f"current/aggregated-file/{dataset_dict["reporting_org_short_name"]}/{dataset_dict["short_name"]}"
-                    )
-                )
-            )
-            if not stats_json:
-                stats_json = DEFAULT_STATS_JSON
-            try:
-                dataset = Dataset(
-                    id=dataset_dict["id"],
-                    reporting_org=ReportingOrg.objects.get(short_name=dataset_dict["reporting_org_short_name"]),
-                    short_name=dataset_dict["short_name"],
-                    source_url=dataset_dict["source_url"],
-                    stats_json=stats_json,
-                    metadata_json=dataset_dict,
-                )
-                dataset.save()
-            except ReportingOrg.DoesNotExist:
-                print("Publisher", dataset_dict["reporting_org_short_name"], "not found")
+        with transaction.atomic():
+            import_reporting_org_metadata()
+            import_dataset_metadata()
+        #with transaction.atomic():
+        #    import_stats()
