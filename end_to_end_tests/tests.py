IATI/IATI-Dashboard

commit 7dcbcc744223c7de21de8d4ccd5dbb31716e577f

feat(jinja2): Replace http links with https
https://github.com/IATI/IATI-Dashboard/issues/736

iati_dashboard/ui/jinja2.py
@@ -25,18 +25,18 @@ def xpath_to_url(path):
     # remove conditions
     path = re.sub(r"\[[^]]+\]", "", path)
     if path.startswith("iati-activity"):
-        url = "http://iatistandard.org/activity-standard/iati-activities/" + path.split("@")[0]
+        url = "https://iatistandard.org/activity-standard/iati-activities/" + path.split("@")[0]
     elif path.startswith("iati-organisation"):
-        url = "http://iatistandard.org/organisation-standard/iati-organisations/" + path.split("@")[0]
+        url = "https://iatistandard.org/organisation-standard/iati-organisations/" + path.split("@")[0]
     else:
-        url = "http://iatistandard.org/activity-standard/iati-activities/iati-activity/" + path.split("@")[0]
+        url = "https://iatistandard.org/activity-standard/iati-activities/iati-activity/" + path.split("@")[0]
     if "@" in path:
         url += "#attributes"
     return url
 
 
 def linkurl(url, link_text=None):
-    if url.startswith("http://") or url.startswith("https://"):
+    if url.startswith("https://") or url.startswith("https://"):
         return format_html('<a href="{}" rel="noopener">{}</a>', url, link_text or url)
     else:
         return link_text or url

iati_dashboard/jinja2/codelist.html
@@ -6,7 +6,7 @@
 <p class="dashboard-page-heading__lead">Who uses <code class="iati-code">{{ codelist_mapping[major_version].get(element) }}</code> in <code class="iati-code"><a href="{{ element|xpath_to_url }}">{{ element }}</a></code>?</p>
 <p class="dashboard-page-heading__lead">(for files published to version {{ major_version }}.xx of the standard)</p>
 <p style="float:right">(<a href="{{ stats_url }}/current/inverted-publisher/codelist_values.json">This page in JSON format</a>)</p>
-<p>Values should be on the <code class="iati-code"><a href="{% if major_version=='2' %}http://iatistandard.org/codelists/{% else %}https://iatistandard.org/105/codelists/{% endif %}{{ codelist_mapping[major_version].get(element) }}/">{{ codelist_mapping[major_version].get(element) }}</a></code> codelist.</p>
+<p>Values should be on the <code class="iati-code"><a href="{% if major_version=='2' %}https://iatistandard.org/codelists/{% else %}https://iatistandard.org/105/codelists/{% endif %}{{ codelist_mapping[major_version].get(element) }}/">{{ codelist_mapping[major_version].get(element) }}</a></code> codelist.</p>
 {% endblock %}
 
 {% block content %}

iati_dashboard/jinja2/codelists.html
@@ -30,9 +30,9 @@
                   {% for i, (element, values) in func.enumerate(current_stats.inverted_publisher.codelist_values_by_major_version[major_version].items()) %}
                     <tr>
                       <td><a href="{{ url('dash-exploringdata-codelists-detail', args=[major_version, slugs.codelist[major_version].by_i[i]]) }}">{{ element }}</a></td>
-                      <td><a href="http://iatistandard.org/codelists/{{ codelist_mapping[major_version].get(element) }}">{{ codelist_mapping[major_version].get(element) }}</a></td>
+                      <td><a href="https://iatistandard.org/codelists/{{ codelist_mapping[major_version].get(element) }}">{{ codelist_mapping[major_version].get(element) }}</a></td>
                       <td><a href="{{ url('dash-exploringdata-codelists-detail', args=[major_version, slugs.codelist[major_version].by_i[i]]) }}">{{ values|length }}</a></td>
-                      <td><a href="http://iatistandard.org/codelists/{{ codelist_mapping[major_version].get(element) }}">{{ codelist_sets[major_version].get(codelist_mapping[major_version].get(element))|length }}</a></td>
+                      <td><a href="https://iatistandard.org/codelists/{{ codelist_mapping[major_version].get(element) }}">{{ codelist_sets[major_version].get(codelist_mapping[major_version].get(element))|length }}</a></td>
                       {% with codes=func.sorted(codelist_sets[major_version].get(codelist_mapping[major_version].get(element)).intersection(func.get_codelist_values(values))) %}
                       <td><a href="{{ url('dash-exploringdata-codelists-detail', args=[major_version, slugs.codelist[major_version].by_i[i]]) }}">{{ codes|length }}</a></td>
                       {% endwith %}

iati_dashboard/jinja2/comprehensiveness_base.html
@@ -225,7 +225,7 @@ Else
           <td>Core</td>
           <td>Version</td>
           <td>iati-activities/@version must exist for the file the activity is in</td>
-          <td>iati-activities/@version must be on the <a href="http://iatistandard.org/codelists/Version/">Version codelist</a></td>
+          <td>iati-activities/@version must be on the <a href="https://iatistandard.org/codelists/Version/">Version codelist</a></td>
         </tr>
         <tr>
           <td>Core</td>
@@ -261,7 +261,7 @@ Else
           <td>Core</td>
           <td>Status</td>
           <td>Activity must contain an activity-status element</td>
-          <td>activity-status/@code must be on the <a href="http://iatistandard.org/codelists/ActivityStatus/">ActivityStats codelist</a></td>
+          <td>activity-status/@code must be on the <a href="https://iatistandard.org/codelists/ActivityStatus/">ActivityStats codelist</a></td>
         </tr>
         <tr>
           <td>Core</td>
@@ -300,7 +300,7 @@ Else
           <td>Financials</td>
           <td>Transaction - Currency</td>
           <td>All transactions must have value/@currency OR the activity must have a @default-currency attribute. All transactions must have value/@value-date</td>
-          <td>All currency values must be on the <a href="http://iatistandard.org/codelists/Currency/">Currency codelist</a>. value/@value-date must be valid <code class="iati-code">xs:date</code>.</td>
+          <td>All currency values must be on the <a href="https://iatistandard.org/codelists/Currency/">Currency codelist</a>. value/@value-date must be valid <code class="iati-code">xs:date</code>.</td>
         </tr>
         <tr>
           <td>Financials</td>
@@ -312,7 +312,7 @@ Else
           <td>Financials</td>
           <td>Budget</td>
           <td>Activity must have at least 1 budget element OR the activity must have the budget-not-provided attribute AND no budget elements</td>
-          <td>Each budget element must contain period-start/@iso-date and period-end/@iso-date and value/@value-date that are valid <code class="iati-code">xs:date</code>s AND a value element that is a valid <code class="iati-code">xs:decimal</code> OR the activity element must have a valid iati-activity/@budget-not-provided attribute under the <a href="http://iatistandard.org/codelists/BudgetNotProvided/">BudgetNotProvided codelist</a> AND no budget elements</td>
+          <td>Each budget element must contain period-start/@iso-date and period-end/@iso-date and value/@value-date that are valid <code class="iati-code">xs:date</code>s AND a value element that is a valid <code class="iati-code">xs:decimal</code> OR the activity element must have a valid iati-activity/@budget-not-provided attribute under the <a href="https://iatistandard.org/codelists/BudgetNotProvided/">BudgetNotProvided codelist</a> AND no budget elements</td>
         </tr>
         {% endblock %}
 
@@ -339,7 +339,7 @@ Else
           <td>Value added</td>
           <td>DAC Sectors</td>
           <td>At least 1 sector where @vocabulary is <code class="iati-code">DAC</code> or <code class="iati-code">DAC-3</code> (1.xx) or <code class="iati-code">1</code> or <code class="iati-code">2</code> (2.xx), must be reported at activity level, unless there is no @vocabulary attribute, in which case DAC/1 is assumed. If there is no DAC sector element at activity level it must be reported within all transactions contained within that activity.</td>
-          <td>Must be valid code on the <a href="http://iatistandard.org/codelists/Sector/">DAC</a> or <a href="http://iatistandard.org/codelists/SectorCategory/">DAC-3</a> lists as appropriate.</td>
+          <td>Must be valid code on the <a href="https://iatistandard.org/codelists/Sector/">DAC</a> or <a href="https://iatistandard.org/codelists/SectorCategory/">DAC-3</a> lists as appropriate.</td>
         </tr>
         <tr>
           <td>Value added</td>
@@ -357,7 +357,7 @@ Else
           <td>Value added</td>
           <td>Aid Type</td>
           <td>Activity must contain either i) a value in <code class="iati-code">default-aid-type/@code</code> or ii) each transaction must contain a value in <code class="iati-code">transaction/aid-type/@code</code>.</td>
-          <td>Must be valid code on the <a href="http://iatistandard.org/codelists/AidType/">AidType codelist</a>.</td>
+          <td>Must be valid code on the <a href="https://iatistandard.org/codelists/AidType/">AidType codelist</a>.</td>
         </tr>
         <tr>
           <td>Value added</td>

iati_dashboard/jinja2/comprehensiveness_financials.html
@@ -23,7 +23,7 @@ Table of Financial values
 
 {% block narrative_text %}
 	<h3>Transaction - Commitment</h3>
-	<p>For the data in the chosen hierarchy, the percentage of all current activities that contain at least one transaction of type <code class="iati-code">Commitment</code> or <code class="iati-code">Incoming Commitment</code>.  The hierarchy chosen for this calculation is the highest hierarchy that contains an above-defined commitment tranaction. Only one hierarchy is selected due in line with <a href="http://support.iatistandard.org/entries/40759343-General-rules-for-multi-level-reporting">stated IATI rules on multi-level reporting</a>.
+	<p>For the data in the chosen hierarchy, the percentage of all current activities that contain at least one transaction of type <code class="iati-code">Commitment</code> or <code class="iati-code">Incoming Commitment</code>.  The hierarchy chosen for this calculation is the highest hierarchy that contains an above-defined commitment tranaction. Only one hierarchy is selected due in line with <a href="https://support.iatistandard.org/entries/40759343-General-rules-for-multi-level-reporting">stated IATI rules on multi-level reporting</a>.
 
 	<h3>Transaction - Disbursement or Expenditure</h3>
 	<p>For the data at the reporting orgs' lowest hierarchy, the percentage of current activities that contain at least one transaction of type <code class="iati-code">Disbursement</code> or <code class="iati-code">Expenditure</code>.</p>

iati_dashboard/jinja2/element.html
@@ -94,7 +94,7 @@
               {% if datasets %}
                 <tr><td id="files_{{ publisher }}"><a href="{{ url('dash-headlines-publisher-detail', args=[publisher]) }}">{{ publisher }}</a></td><td>
                 {% for dataset in datasets.keys() %}
-                    <a href="http://iatiregistry.org/dataset/{{ dataset[:-4] }}">{{ dataset[:-4] }}</a>
+                    <a href="https://iatiregistry.org/dataset/{{ dataset[:-4] }}">{{ dataset[:-4] }}</a>
                 {% endfor %}
                 </td></tr>
               {% endif %}

iati_dashboard/jinja2/faq.html
@@ -21,7 +21,7 @@
 <p>Usually, there is a short period of time between the two. However, we do track this as sometimes things break and the site may fail to regenerate. If you spot something, please <a href="https://iatistandard.org/en/guidance/get-support/">contact us</a> to let us know.</p>
 
 <h2>What do the Publishing Statistics numbers mean?</h2>
-<p>The tables show how timely, forward looking and comprehensive a publisher's IATI data is.  A summary of this information is displayed on the <a href="http://publishingstats.iatistandard.org">Summary Statistics</a> page.</p>
+<p>The tables show how timely, forward looking and comprehensive a publisher's IATI data is.  A summary of this information is displayed on the <a href="https://publishingstats.iatistandard.org">Summary Statistics</a> page.</p>
 <p>The other tabs outline how this mark is calculated and the different assessments included.</p>
 <p>Only the timeliness tables track an organisation's publishing over time. If the Publishing Statistics doesn't pick up your data one month we are unable to back fill the table.</p>
 

iati_dashboard/jinja2/versions.html
@@ -31,7 +31,7 @@
                   {% if datasets %}
                   <tr><td id="files_{{ publisher }}"><a href="{{ url('dash-headlines-publisher-detail', args=[publisher]) }}">{{ publisher_name[publisher] }}</a></td><td>
                   {% for dataset in datasets.keys() %}
-                      <a href="http://iatiregistry.org/dataset/{{ dataset[:-4] }}">{{ dataset[:-4] }}</a>
+                      <a href="https://iatiregistry.org/dataset/{{ dataset[:-4] }}">{{ dataset[:-4] }}</a>
                   {% endfor %}
                   </td></tr>
                   {% endif %}

commit e3521c3b03914f961c5138ce6e6d595c785713f1

fix(jinja filters): Turn insecure http links into urls correctly
This was accidentally broken in https://github.com/IATI/IATI-Dashboard/commit/7dcbcc744223c7de21de8d4ccd5dbb31716e577f

end_to_end_tests/tests.py
@@ -88,11 +88,16 @@ def test_reporting_orgs(selenium):
     assert recipient_countries_cell.text == "1"
     href = recipient_countries_cell.find_element("tag name", "a").get_attribute("href")
     assert href.endswith("/publishers/in_ao_1/#p_countries")
+
     selenium.get(href)
+
     recipient_countries_panel = selenium.find_element("css selector", "#p_countries")
     assert "Recipient Countries" in recipient_countries_panel.text
     assert "AO" in recipient_countries_panel.text
     assert "Angola" in recipient_countries_panel.text
+
+    source_url_1 = selenium.find_element("link text", "Source Url")
+    assert source_url_1.get_attribute("href") == "http://example.com/1"
 
 
 def test_reporting_orgs_filter(selenium):

iati_dashboard/ui/jinja2.py
@@ -36,7 +36,7 @@ def xpath_to_url(path):
 
 
 def linkurl(url, link_text=None):
-    if url.startswith("https://") or url.startswith("https://"):
+    if url.startswith("http://") or url.startswith("https://"):
         return format_html('<a href="{}" rel="noopener">{}</a>', url, link_text or url)
     else:
         return link_text or url